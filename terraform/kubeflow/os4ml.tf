977e05e537514eefb0a3858d15f62b41c539f1da
Tim Köhler <tk@wogra.com>
2021-12-06 09:48:26 +0100

    add rbac and istio rules to os4ml frontend

--- terraform/kubeflow/os4ml.tf
+++ terraform/kubeflow/os4ml.tf
@@ -1,9 +1,9 @@
 data "kustomization_build" "os4ml" {
-  path = "../../services/frontend/kubernetes/base"
+  path = "../../services/frontend/kubernetes/overlays/istio"
 }
 
 resource "kustomization_resource" "os4ml" {
-  for_each = data.kustomization_build.os4ml.ids
+  for_each = var.deploy_os4ml ? data.kustomization_build.os4ml.ids : []
   manifest = data.kustomization_build.os4ml.manifests[each.value]
 
   depends_on = [
