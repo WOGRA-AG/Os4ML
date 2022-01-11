0d21c28ac93b2fc3100ff006c3002ceb60e58980
Thomas Fraunholz <tf@wogra.com>
2022-01-11 11:02:01 +0100

    add dependency of frontend to kubeflow

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -5,4 +5,8 @@ data "kustomization_build" "frontend_build" {
 resource "kustomization_resource" "frontend" {
   for_each = var.deploy_frontend ? data.kustomization_build.frontend_build.ids : []
   manifest = data.kustomization_build.frontend_build.manifests[each.value]
+
+  depends_on = [
+    module.kubeflow
+  ]
 }
