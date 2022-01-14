d21c58c9e220d54c1b45bdda9e6b7f80c5b4c686
Thomas Fraunholz <tf@wogra.com>
2022-01-14 10:25:39 +0100

    add version for kubectl

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "kbst/kustomization"
       version = ">= 0.7.0"
     }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.13.1"
+    }
   }
 
   required_version = "~> 1.1"
