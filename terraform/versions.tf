075fda37bad17f395a09c73a77007fc3347ec382
Dennis Rall <dennis.rall@wogra.com>
2022-02-07 10:09:35 +0100

    add nvidia driver installer with kustomization provider

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,7 +4,10 @@ terraform {
       source  = "kbst/kustomization"
       version = ">= 0.7.0"
     }
-
+    curl = {
+      source  = "anschoewe/curl"
+      version = "0.1.3"
+    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.13.1"
