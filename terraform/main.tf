9f7c405c6881220adbe95901a87bc7b6d63e32c1
Thomas Fraunholz <tf@wogra.com>
2022-02-24 08:42:56 +0100

    update kubeflow module to v0.2.1

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
 
 module "kubeflow" {
   source  = "WOGRA-AG/kubeflow/kustomization"
-  version = "0.2.0"
+  version = "0.2.1"
 
   deploy_istio       = true
   deploy_pipelines   = true
@@ -17,7 +17,7 @@ module "kubeflow" {
   deploy_tensorboard = false
   deploy_volumes     = false
   deploy_serving     = false
-  istio_ingress      = false
+  istio_ingress_load_balancer      = false
   dex_user_email     = var.user_email
   dex_user_hash      = var.user_hash
   dex_user_name      = var.user_name
