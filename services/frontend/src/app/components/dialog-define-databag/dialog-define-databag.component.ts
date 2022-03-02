30537cb690c3ad64a3d67d19da8687be621153e6
Tim Köhler <tk@wogra.com>
2022-03-02 12:24:50 +0100

    clean up dialog-define-databag

--- services/frontend/src/app/components/dialog-define-databag/dialog-define-databag.component.ts
+++ services/frontend/src/app/components/dialog-define-databag/dialog-define-databag.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {MatDialogRef} from '@angular/material/dialog';
 import {DialogDynamicComponent} from '../dialog-dynamic/dialog-dynamic.component';
 import {DialogAddDatabagComponent} from '../dialog-add-databag/dialog-add-databag.component';
-import {FormControl, Validators} from '@angular/forms';
 import {MlTypes} from '../../models/ml-types';
 import {Databag, ObjectstoreService} from '../../../../build/openapi/objectstore';
 
@@ -14,25 +13,16 @@ import {Databag, ObjectstoreService} from '../../../../build/openapi/objectstore
 export class DialogDefineDatabagComponent {
   mlTypesArray = Object.keys(MlTypes);
   uuid: string = '';
-  name: FormControl = new FormControl('',
-    [Validators.required, Validators.maxLength(50), Validators.minLength(3)]
-  );
   databag: Databag = {};
 
   constructor(private dialogRef: MatDialogRef<DialogDynamicComponent>, private objectstoreService: ObjectstoreService) {
     this.uuid = dialogRef.componentInstance.data.uuid;
     this.objectstoreService.getDatabagByBucketName(this.uuid).subscribe((databag: Databag) => {
-      this.name.setValue(databag.databag_name);
       this.databag = databag;
     });
   }
 
-  createBag(): void {
-    if (!this.name.valid) {
-      this.name.markAllAsTouched();
-      return;
-    }
-    this.databag.databag_name = this.name.value
+  onSubmit(): void {
     this.objectstoreService.putDatabagByBucketName(this.uuid, this.databag).subscribe(() => {
       this.dialogRef.close();
     });
