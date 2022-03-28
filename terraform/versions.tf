db3af4892c496487ba762df55d4bfa38acec0321
Tim Köhler <tk@wogra.com>
2022-03-28 09:22:38 +0200

    add influx

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -16,6 +16,10 @@ terraform {
       source  = "hashicorp/null"
       version = "3.1.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.4.1"
+    }
   }
 
   required_version = "~> 1.1"
