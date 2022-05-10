f8c976da44fe1bd25a45716aed43a3603538e84c
Dominik Koziol <dk@wogra.com>
2022-05-10 12:54:00 +0200

    Update black and add openapi to docker dev stage

--- services/job-manager/src/services/__init__.py
+++ services/job-manager/src/services/__init__.py
@@ -4,6 +4,6 @@ ML_PIPELINE_URL: str = None if os.getenv("ML_PIPELINE_URL") is None else os.gete
 
 ML_PIPELINE_NS: str = None if os.getenv("ML_PIPELINE_NS") is None else os.getenv("ML_PIPELINE_NS")
 
-SOLUTION_CONFIG_FILE_NAME: str = "solution.json" if os.getenv("SOLUTION_CONFIG_FILE_NAME") is None else os.getenv(
-    "SOLUTION_CONFIG_FILE_NAME"
+SOLUTION_CONFIG_FILE_NAME: str = (
+    "solution.json" if os.getenv("SOLUTION_CONFIG_FILE_NAME") is None else os.getenv("SOLUTION_CONFIG_FILE_NAME")
 )
