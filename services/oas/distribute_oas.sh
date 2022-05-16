27b1ee4e6c0787cc90a33fefabeee450b5768f5d
Dominik Koziol <dk@wogra.com>
2022-05-16 15:20:10 +0200

    Add mustache templates to openapi.sh

--- services/oas/distribute_oas.sh
+++ services/oas/distribute_oas.sh
@@ -5,3 +5,5 @@ cp ./shared-oas.yaml ../objectstore-manager/oas/
 cp ./objectstore-manager-oas.yaml ../job-manager/oas/
 cp ./objectstore-manager-oas.yaml ../objectstore-manager/oas/
 cp ./job-manager-oas.yaml ../job-manager/oas/
+cp -r ./templates ../job-manager/oas
+cp -r ./templates ../objectstore-manager/oas
