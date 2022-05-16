7e649923735da4c2d71d062ed238614401e90398
Dominik Koziol <dk@wogra.com>
2022-05-16 14:46:48 +0200

    Add dockerfile production and cleanup oas

--- services/oas/distribute_oas.sh
+++ services/oas/distribute_oas.sh
@@ -2,3 +2,6 @@
 
 cp ./shared-oas.yaml ../job-manager/oas/
 cp ./shared-oas.yaml ../objectstore-manager/oas/
+cp ./objectstore-manager-oas.yaml ../job-manager/oas/
+cp ./objectstore-manager-oas.yaml ../objectstore-manager/oas/
+cp ./job-manager-oas.yaml ../job-manager/oas/
