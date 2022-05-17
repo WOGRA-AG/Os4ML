85da047c82cff4ff47a24efd2fc0048a61df051a
Dominik Koziol <dk@wogra.com>
2022-05-17 15:44:19 +0200

    Add python api generation by template to Dockerfile

--- services/oas/distribute_oas.sh
+++ services/oas/distribute_oas.sh
@@ -7,3 +7,5 @@ cp ./objectstore-manager-oas.yaml ../objectstore-manager/oas/
 cp ./job-manager-oas.yaml ../job-manager/oas/
 cp -r ./templates ../job-manager/oas
 cp -r ./templates ../objectstore-manager/oas
+cp -r ./templates_client ../objectstore-manager/oas
+cp -r ./templates_client ../objectstore-manager/oas
