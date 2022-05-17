f9a22e9bdce55c9cbeab92c15624748db902be1a
Dominik Koziol <dk@wogra.com>
2022-05-17 16:45:10 +0200

    Remove openapi.sh from objectstore-manager

--- services/objectstore-manager/openapi.sh
+++
@@ -1,9 +0,0 @@
-# -> Original Mustache Templates https://github.com/OpenAPITools/openapi-generator/tree/master/modules/openapi-generator/src/main/resources/python-fastapi
-# -> Template Customization Tutorial https://duongnt.com/openapi-generator-mustache-customize/
-
-cd src && openapi-generator-cli generate \
--i ../oas/objectstore-manager-oas.yaml \
--g python \
---package-name build.openapi_client \
--o . \
---global-property=apiTests=false,apiDocs=false,modelTests=false,modelDocs=false
