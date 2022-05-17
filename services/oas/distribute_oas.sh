6898d5e0ffc8499f99bce3c66972bd5813a5950e
Dominik Koziol <dk@wogra.com>
2022-05-17 16:43:42 +0200

    Remove objectstore related code

--- services/oas/distribute_oas.sh
+++ services/oas/distribute_oas.sh
@@ -2,10 +2,3 @@
 
 cp ./shared-oas.yaml ../job-manager/oas/
 cp ./shared-oas.yaml ../objectstore-manager/oas/
-cp ./objectstore-manager-oas.yaml ../job-manager/oas/
-cp ./objectstore-manager-oas.yaml ../objectstore-manager/oas/
-cp ./job-manager-oas.yaml ../job-manager/oas/
-cp -r ./templates ../job-manager/oas
-cp -r ./templates ../objectstore-manager/oas
-cp -r ./templates_client ../objectstore-manager/oas
-cp -r ./templates_client ../objectstore-manager/oas
