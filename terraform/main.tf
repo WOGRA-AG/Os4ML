68d58dc8c3ceb34a7cfe84fc96366ecbe94d77b5
Tim Köhler <tk@wogra.com>
2022-05-25 01:44:38 +0200

    add tls to os4ml and create separate routing

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
 
 module "kubeflow" {
   source  = "WOGRA-AG/kubeflow/kustomization"
-  version = "0.2.2"
+  version = "0.3.1"
 
   deploy_istio                = true
   deploy_pipelines            = true
@@ -17,9 +17,13 @@ module "kubeflow" {
   deploy_tensorboard          = false
   deploy_volumes              = false
   deploy_serving              = false
-  istio_ingress_load_balancer = false
+  istio_ingress_load_balancer = var.cluster_provisioner == "gke"
   dex_user_email              = var.user_email
   dex_user_hash               = var.user_hash
   dex_user_name               = var.user_name
   dex_user_id                 = var.user_id
+  kubeflow_dns_name           = var.kubeflow_dns_name
+  provide_tls                 = var.cluster_provisioner == "gke"
+  external_ip                 = var.external_ip
+  letsencrypt_mail            = var.letsencrypt_mail
 }
