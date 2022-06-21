3ffdf40b8a3adc988e123c66d3343ef6419e6a39
Dennis Rall <dennis.rall@wogra.com>
2022-06-21 09:18:28 +0200

    Use latest image tag

--- templates/components/images.py
+++ templates/components/images.py
@@ -1,3 +1,3 @@
-python_image = "gitlab-registry.wogra.com/developer/wogra/os4ml/python:166-cleaner-shared-models-for-pipeline-templates"
-pandas_image = "gitlab-registry.wogra.com/developer/wogra/os4ml/pandas:166-cleaner-shared-models-for-pipeline-templates"
-ludwig_image = "gitlab-registry.wogra.com/developer/wogra/os4ml/ludwig:166-cleaner-shared-models-for-pipeline-templates"
+python_image = "gitlab-registry.wogra.com/developer/wogra/os4ml/python:latest"
+pandas_image = "gitlab-registry.wogra.com/developer/wogra/os4ml/pandas:latest"
+ludwig_image = "gitlab-registry.wogra.com/developer/wogra/os4ml/ludwig:latest"
