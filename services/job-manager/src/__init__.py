86e6d428bf673aa118511988ce439514f39ea328
Dennis Rall <dennis.rall@wogra.com>
2022-06-29 09:22:53 +0200

    Delete duplicated code

--- services/job-manager/src/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ML_PIPELINE_URL: str = os.getenv("ML_PIPELINE_URL", default=None)
-
-ML_PIPELINE_NS: str = os.getenv("ML_PIPELINE_NS", default=None)
-
-SOLUTION_CONFIG_FILE_NAME: str = os.getenv(
-    "SOLUTION_CONFIG_FILE_NAME", default="solution.json"
-)
