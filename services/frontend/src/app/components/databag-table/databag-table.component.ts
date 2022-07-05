19bb6a893666350c8256d0e28a2609003bb4a7a1
Dominik Koziol <dk@wogra.com>
2022-07-05 16:03:15 +0200

    Add filtering onChange and update table styles

--- services/frontend/src/app/components/databag-table/databag-table.component.ts
+++ services/frontend/src/app/components/databag-table/databag-table.component.ts
@@ -18,6 +18,7 @@ import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {DialogEditDatabagComponent} from '../dialog-edit-databag/dialog-edit-databag.component';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-databag-table',
@@ -32,6 +33,7 @@ export class DatabagTableComponent implements AfterViewInit {
 
   dataSource: MatTableDataSource<Databag> = new MatTableDataSource<Databag>();
 
+  filterValue = '';
   intervalSub: Subscription;
   displayedColumns: string[] = [
     'databagName', 'fileName', 'datasetType', 'numberColumns', 'numberRows',
@@ -40,7 +42,10 @@ export class DatabagTableComponent implements AfterViewInit {
 
   constructor(public dialog: MatDialog, private router: Router, private activatedRoute: ActivatedRoute) {
     this.intervalSub = interval(10000).subscribe(() => {
-      router.navigate(['.'], {relativeTo: activatedRoute}).then(() => this.refreshDatasource());
+      router.navigate(['.'], {relativeTo: activatedRoute}).then(() => {
+        this.refreshDatasource();
+        this.applyFilter();
+      });
     });
   }
 
@@ -58,9 +63,13 @@ export class DatabagTableComponent implements AfterViewInit {
     this.dataSource.sort = this.sort!;
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  updateAndApplyFilterValue(event: Event) {
+    this.filterValue = (event.target as HTMLInputElement).value;
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    this.dataSource.filter = this.filterValue.trim().toLowerCase();
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
---
+++ services/frontend/src/app/pipes/localized-date.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { DatePipe } from '@angular/common';
+
+@Pipe({
+  name: 'localizedDate'
+})
+export class LocalizedDatePipe implements PipeTransform {
+
+  constructor(private translateService: TranslateService) {
+  }
+
+  transform(value: any, pattern: string = 'longDate'): any {
+    const datePipe: DatePipe = new DatePipe(this.translateService.currentLang);
+    return datePipe.transform(value, pattern);
+  }
+
+}
