f98dc2946571690e58799d2b1ffec7e04f71a987
Dominik Koziol <dk@wogra.com>
2022-07-19 13:01:23 +0200

    Enhance testing for getDatabagByRunId

--- services/objectstore-manager/tests/integration/test_databag.py
+++ services/objectstore-manager/tests/integration/test_databag.py
@@ -64,3 +64,7 @@ async def test_get_databag_by_run_id():
         run_id="os4ml_unique_run_id", _controller=mock_objectstore_controller,
     )
     assert databag.run_id == "os4ml_unique_run_id"
+    another_databag: Databag = await get_databag_by_run_id(
+        run_id="false_os4ml_unique_run_id", _service=mock_databag_api_service,
+    )
+    assert another_databag is None
