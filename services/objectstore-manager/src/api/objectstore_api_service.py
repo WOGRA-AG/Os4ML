da906d0febb315e23c83e12bc2fc3b03b6791833
Dennis Rall <dennis.rall@wogra.com>
2022-07-19 08:38:10 +0200

    Remove get all solutions from objectstore

--- services/objectstore-manager/src/api/objectstore_api_service.py
+++ services/objectstore-manager/src/api/objectstore_api_service.py
@@ -5,10 +5,8 @@ from fastapi.responses import RedirectResponse
 
 from build.openapi_server.models.bucket import Bucket
 from build.openapi_server.models.item import Item
-from build.openapi_server.models.solution import Solution
 from services import STORAGE_BACKEND
 from services.init_storage_service import storage_services
-from services.solution_service import SolutionService
 from services.storage_service_interface import StorageService
 
 
@@ -22,11 +20,7 @@ class ObjectstoreApiService:
             if storage_service is not None
             else storage_services[STORAGE_BACKEND]()
         )
-        self.solution_service: SolutionService = (
-            SolutionService(storage_service)
-            if storage_service is not None
-            else SolutionService(storage_services[STORAGE_BACKEND]())
-        )
+
 
     def delete_object_by_name(self, bucket_name, object_name) -> None:
         return self.storage_service.delete_item(
@@ -71,9 +65,6 @@ class ObjectstoreApiService:
 
     def post_new_bucket(self, bucket_name) -> Bucket:
         return self.storage_service.create_bucket(bucket_name=bucket_name)
-
-    def get_all_solutions(self) -> List[Solution]:
-        return self.solution_service.get_all_solutions()
 
     def get_all_buckets(self):
         return self.storage_service.list_buckets()
