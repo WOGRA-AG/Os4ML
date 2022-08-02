eeae8936baf41079095d0100cf1b3c011c0ad0a3
Dennis Rall <dennis.rall@wogra.com>
2022-08-02 11:36:35 +0200

    Build oas clients in python dockerfile

--- services/oas/distribute_oas.sh
+++ services/oas/distribute_oas.sh
@@ -9,3 +9,8 @@ cp -r ./templates_client ../job-manager/oas/
 cp ./shared-oas.yaml ../objectstore-manager/oas/
 cp ./objectstore-manager-oas.yaml ../objectstore-manager/oas/
 cp -r ./templates ../objectstore-manager/oas/
+
+cp ./shared-oas.yaml ../../templates/oas/
+cp ./job-manager-oas.yaml ../../templates/oas/
+cp ./objectstore-manager-oas.yaml ../../templates/oas/
+cp -r ./templates_client ../../templates/oas/
