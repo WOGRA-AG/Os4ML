024060eedb1ebadcb52f388271e2bf6adec60ec3
Dominik Koziol <dk@wogra.com>
2022-08-02 16:44:00 +0200

    Add initial upload and define databag table e2e test

--- services/frontend/cypress.config.ts
+++ services/frontend/cypress.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    'baseUrl': 'http://localhost:4200',
+    baseUrl: 'http://localhost:4200',
+    defaultCommandTimeout: 30000,
   },
 });
--- services/frontend/cypress/e2e/spec.cy.ts
+++ services/frontend/cypress/e2e/spec.cy.ts
@@ -7,14 +7,29 @@ describe('Databags', () => {
     cy.title().should('equal', 'Frontend');
   });
 
-  it('add databag button available', () => {
-    cy.get('.button-add-databag').click();
+  it('add and define databag', () => {
     const urlToPaste = 'https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv';
+    // eslint-disable-next-line max-len
+    // Use the .button-add-databag instead of [id='add-databag-button-empty'] if you want to test the workflow with already uploaded databags
+    cy.get('[id=\'add-databag-button-empty\']').click();
     cy.get('[type=\'url\']').focus().type(urlToPaste).blur();
-    cy.get('.main-button').click();
-    cy.get('.main-button', { timeout: 600000 }).click();
-    cy.get('.main-button', { timeout: 600000 }).click();
-    cy.get('table', { timeout: 600000 }).find('tr').eq(0).find('td').eq(0).contains('titanic.csv');
+    cy.get('[id=\'add-databag-button\']').click();
+    cy.get('[id=\'ngForm\']', { timeout: 600000 }).find('.columns').find('.mat-select-value-text').eq(0).find('span').contains('category');
+    cy.get('[id=\'ngForm\']').find('.columns').find('.mat-select-value-text').eq(1).find('span').contains('category');
+    cy.get('[id=\'ngForm\']').find('.columns').find('.mat-select-value-text').eq(2).find('span').contains('text');
+    cy.get('[id=\'ngForm\']').find('.columns').find('.mat-select-value-text').eq(3).find('span').contains('category');
+    cy.get('[id=\'ngForm\']').find('.columns').find('.mat-select-value-text').eq(4).find('span').contains('numerical');
+    cy.get('[id=\'ngForm\']').find('.columns').find('.mat-select-value-text').eq(5).find('span').contains('category');
+    cy.get('[id=\'ngForm\']').find('.columns').find('.mat-select-value-text').eq(6).find('span').contains('category');
+    cy.get('[id=\'ngForm\']').find('.columns').find('.mat-select-value-text').eq(7).find('span').contains('numerical');
+    cy.get('[id=\'define-databag-button\']').click();
+    // eslint-disable-next-line max-len
+    // Use cy.get('[id=\'databag-table\']').find('tbody').find('tr').eq(0).find('td').eq(0).contains('titanic.csv'); if multiple entries are present in the table
+    cy.get('[id=\'databag-table\']').find('tbody').find('tr').find('td').eq(0).contains('titanic.csv');
+    cy.get('[id=\'databag-table\']').find('tbody').find('tr').find('td').eq(1).contains(urlToPaste);
+    cy.get('[id=\'databag-table\']').find('tbody').find('tr').find('td').eq(2).contains('file_url');
+    cy.get('[id=\'databag-table\']').find('tbody').find('tr').find('td').eq(3).contains('8');
+    cy.get('[id=\'databag-table\']').find('tbody').find('tr').find('td').eq(4).contains('887');
+    cy.get('[id=\'databag-table\']').find('tbody').find('tr').find('td').eq(5);
   });
-
 });
