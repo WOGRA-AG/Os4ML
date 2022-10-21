6d331420cfee1c6e5577665487b19847cc915732
Dennis Rall <dennis.rall@wogra.com>
2022-10-21 11:01:31 +0200

    Simpliy distribute oas script

--- services/model-manager/openapi.sh
+++ services/model-manager/openapi.sh
@@ -5,3 +5,19 @@ openapi-generator-cli generate \
 -g python-fastapi \
 -o . \
 --global-property=apiTests=false,apiDocs=false,modelTests=false,modelDocs=false
+
+openapi-generator-cli generate \
+-i ../oas/model-manager-oas.yaml \
+-t ../oas/templates_client \
+-g python \
+--package-name build.model_manager_client \
+-o . \
+--global-property=apiTests=false,apiDocs=false,modelTests=false,modelDocs=false
+
+# openapi-generator-cli generate \
+# -i ../oas/job-manager-oas.yaml \
+# -t ../oas/templates_client \
+# -g python \
+# --package-name build.job_manager_client \
+# -o . \
+# --global-property=apiTests=false,apiDocs=false,modelTests=false,modelDocs=false
