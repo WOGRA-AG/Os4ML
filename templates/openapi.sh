a50fb4fe61c88ac21bf6afc7d634ba97d2d460bf
Dennis Rall <dennis.rall@wogra.com>
2022-11-08 09:15:00 +0100

    Change templates openapi.sh

--- templates/openapi.sh
+++ templates/openapi.sh
@@ -1,8 +1,19 @@
+#!/bin/sh
+
 docker run --rm \
   -v $PWD:/local/ openapitools/openapi-generator-cli:v6.2.0 generate \
   -i /local/oas/model-manager-oas.yaml \
   -t /local/oas/templates_client \
   -g python \
   --package-name src.build.model_manager_client \
+  -o /local \
+  --global-property=apiTests=false,apiDocs=false,modelTests=false,modelDocs=false
+
+docker run --rm \
+  -v $PWD:/local/ openapitools/openapi-generator-cli:v5.4.0 generate \
+  -i /local/oas/objectstore-manager-oas.yaml \
+  -t /local/oas/templates_client \
+  -g python \
+  --package-name src.build.objectstore_client \
   -o /local \
   --global-property=apiTests=false,apiDocs=false,modelTests=false,modelDocs=false
