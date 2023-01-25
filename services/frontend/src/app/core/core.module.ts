4a0c13f4721e4a9cd31f87a16e95c83e2ce520f0
Dennis Rall <dennis.rall@wogra.com>
2023-01-25 12:51:17 +0100

    Delete page not found component

--- services/frontend/src/app/core/core.module.ts
+++ services/frontend/src/app/core/core.module.ts
@@ -1,21 +1,10 @@
 import {NgModule} from '@angular/core';
-import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
-import {SharedModule} from '../shared/shared.module';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {ErrorInterceptor} from './interceptors/error.interceptor';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 
 @NgModule({
-  declarations: [
-    PageNotFoundComponent
-  ],
-  exports: [
-    PageNotFoundComponent,
-  ],
-  imports: [
-    SharedModule,
-  ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
