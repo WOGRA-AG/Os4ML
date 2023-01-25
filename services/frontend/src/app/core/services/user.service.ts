187e9d4402e069e4cfaccfb73af77f44538ed8e3
Dennis Rall <dennis.rall@wogra.com>
2023-01-25 15:09:27 +0100

    Use properties and fix naming in user service

--- services/frontend/src/app/core/services/user.service.ts
+++ services/frontend/src/app/core/services/user.service.ts
@@ -8,13 +8,13 @@ import {Router} from '@angular/router';
 })
 export class UserService {
 
-  currentUser$: Observable<User>;
-  private readonly currentTokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  private readonly currentTokenSubject$: BehaviorSubject<string> = new BehaviorSubject<string>('');
   private readonly jwtTokenStorage = 'JWT_TOKEN';
 
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) {
-    this.currentUser$ = this.currentTokenSubject.pipe(
+  get currentUser$(): Observable<User> {
+    return this.currentTokenSubject$.pipe(
       filter(token => !!token),
       map(token => {
         const encodedPayload = token.split('.')[1];
@@ -33,17 +33,17 @@ export class UserService {
   }
 
   get currentToken$(): Observable<string> {
-    return this.currentTokenSubject.asObservable();
+    return this.currentTokenSubject$.asObservable();
   }
 
   updateUser(jwtTokenString: string): void {
     localStorage.setItem(this.jwtTokenStorage, jwtTokenString);
-    this.currentTokenSubject.next(jwtTokenString);
+    this.currentTokenSubject$.next(jwtTokenString);
   }
 
   logout(): void {
     localStorage.removeItem(this.jwtTokenStorage);
-    this.currentTokenSubject.next('');
+    this.currentTokenSubject$.next('');
     this.router.navigateByUrl('/logout');
   }
 }
