0dbd46f9f3561b4e017c84772a0fadcd4558c11d
Tim Köhler <tk@wogra.com>
2023-03-10 08:21:02 +0100

    fix e2e tests

--- services/frontend/cypress/e2e/spec.cy.ts
+++ services/frontend/cypress/e2e/spec.cy.ts
@@ -10,11 +10,11 @@ describe('Databags', () => {
     cy.get(':nth-child(1) > .pure-material-textfield-outlined > span').click();
     cy.get('#username').clear();
     // add username to run locally
-    cy.get('#username').type(Cypress.env('TEST_USER'));
+    cy.get('#username').type('tim');
     cy.get(':nth-child(2) > .pure-material-textfield-outlined > span').click();
     cy.get('#password').clear();
     // add password to run locally
-    cy.get('#password').type(Cypress.env('TEST_PASSWORD'));
+    cy.get('#password').type('1Qay2Wsx');
     cy.get('#kc-login > span').click();
   });
 
@@ -43,7 +43,7 @@ describe('Databags', () => {
     }
     cy.visit('/databag');
     waitUntilElementExists('#add-databag-button-empty');
-    cy.get('#add-databag-button-empty > .mat-button-wrapper').click();
+    cy.get('#add-databag-button-empty').click();
     cy.get('#mat-input-0').click();
     cy.get('#mat-input-0').clear();
     cy.get('#mat-input-0').type('e2e test databag');
@@ -52,20 +52,20 @@ describe('Databags', () => {
       .selectFile('cypress/fixtures/titanic.xls');
     cy.get('#add-databag-main-button').click();
     cy.get(
-      '.mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix',
+      '.mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix',
       { timeout: databagTimeout }
     )
-      .get('.mat-select-min-line ', { timeout: databagTimeout })
+      .get('.mat-mdc-select-min-line ', { timeout: databagTimeout })
       .contains('category');
     cy.get(
-      '.mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix'
+      '.mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix'
     )
-      .get('.mat-select-min-line ')
+      .get('.mat-mdc-select-min-line ')
       .contains('category');
     cy.get(
-      '.mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix'
+      '.mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix'
     )
-      .get('.mat-select-min-line ')
+      .get('.mat-mdc-select-min-line ')
       .contains('text');
     cy.get('#define-databag-button').click();
     cy.wait(2000);
@@ -73,19 +73,19 @@ describe('Databags', () => {
 
   it('train ludwig solution', () => {
     cy.visit('/dashboard');
-    cy.get('#add-solution-button-empty > .mat-button-wrapper').click();
+    cy.get('#add-solution-button-empty').click();
     cy.get(
-      'app-selectable-list.ng-star-inserted > .mat-list > :nth-child(2) > .mat-list-item-content > app-list-item > div > .mat-subheading-2',
+      'app-selectable-list.ng-star-inserted > .mat-mdc-list > :nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > app-list-item > .list-item-container > .mat-subtitle-2',
       { timeout: databagTimeout }
     ).should('have.text', ' category ');
     cy.get(
-      'app-selectable-list.ng-star-inserted > .mat-list > :nth-child(2) > .mat-list-item-content'
+      'app-selectable-list.ng-star-inserted > .mat-mdc-list > :nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content'
     ).click();
     cy.get('#define-output-next-button').click();
     cy.get('#define-solver-name-input').clear();
     cy.get('#define-solver-name-input').type('e2e test ludwig solver');
     cy.get(
-      'app-choose-solver > app-selectable-list > .mat-list > .mat-list-item > .mat-list-item-content'
+      'app-choose-solver > app-selectable-list > .mat-mdc-list > :nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content'
     ).click();
     cy.get('#define-solver-next-button').click();
     cy.get('.status-column > .done', { timeout: solutionTimeout });
@@ -106,14 +106,12 @@ describe('Databags', () => {
   it('rename databag', () => {
     cy.visit('/databag');
     cy.get(
-      '.sidenav-container > :nth-child(1) > .mat-nav-list > :nth-child(2) > .mat-list-item-content'
+      '.sidenav-container > :nth-child(1) > .mat-mdc-nav-list > :nth-child(2) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content'
     ).click();
     cy.get('.databag-list-item > :nth-child(2)').click();
     cy.get('#mat-input-0').clear();
     cy.get('#mat-input-0').type('renamed-titanic.xls');
-    cy.get(
-      '.mat-dialog-actions > .mat-raised-button > .mat-button-wrapper'
-    ).click();
+    cy.get('.mat-mdc-dialog-actions > .mdc-button--raised').click();
     cy.get(':nth-child(1) > .mat-body-2').should(
       'have.text',
       'renamed-titanic.xls'
@@ -123,15 +121,13 @@ describe('Databags', () => {
   it('rename solution', () => {
     cy.visit('/dashboard');
     cy.get(
-      '.ng-star-inserted > .mat-list-item-content > .nav-item-extended'
+      '#bag-list > :nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended'
     ).click();
-    cy.get('.solution-list-item > :nth-child(3)').click();
+    cy.get('.solution-list-item > :nth-child(1)').click();
     cy.get('#mat-input-0').clear();
     cy.get('#mat-input-0').type('Renamed Solution');
-    cy.get(
-      '.mat-dialog-actions > [type="submit"] > .mat-button-wrapper'
-    ).click();
-    cy.get('.solution-list-item > :nth-child(1) > .mat-body-2').should(
+    cy.get('.mat-mdc-dialog-actions > [type="submit"]').click();
+    cy.get('.solution-list-item > :nth-child(1) > :nth-child(2)').should(
       'have.text',
       'Renamed Solution'
     );
@@ -139,9 +135,9 @@ describe('Databags', () => {
 
   it('delete solution', () => {
     cy.visit('/dashboard');
-    cy.get('.mat-subheading-2').click();
+    cy.get('.mat-subtitle-2').click();
     cy.get('.solution-list-item > :nth-child(1)').click();
-    cy.get('.delete-button > .mat-button-wrapper').click();
+    cy.get('.delete-button').click();
     cy.get(
       '.ng-star-inserted > app-dialog-section > .dialog-element > .dialog-element-content'
     ).should(
@@ -149,19 +145,22 @@ describe('Databags', () => {
       ' Are you sure you want to delete this solution? All data will be lost!\n'
     );
     cy.get(
-      '#mat-dialog-1 > .ng-star-inserted > .mat-dialog-actions > .mat-stroked-button > .mat-button-wrapper'
+      'app-popup-confirm > .mat-mdc-dialog-actions > .mdc-button--outlined'
     )
       .wait(deleteTimeout)
       .click();
     cy.wait(deleteTimeout);
-    cy.get('h5').should('have.text', 'Analyze your Data with Machine Learning');
+    cy.get('h1.primary').should(
+      'have.text',
+      'Analyze your Data with Machine Learning'
+    );
   });
 
   it('delete databag', () => {
     cy.visit('/dashboard');
-    cy.get(':nth-child(2) > .mat-list-item-content > .nav-caption').click();
+    cy.get('.mat-mdc-nav-list > :nth-child(2)').click();
     cy.get('.databag-list-item > :nth-child(1)').click();
-    cy.get('.mat-stroked-button > .mat-button-wrapper').click();
+    cy.get('.mdc-button--outlined').click();
     cy.get(
       '.ng-star-inserted > app-dialog-section > .dialog-element > .dialog-element-content'
     ).should(
@@ -169,82 +168,85 @@ describe('Databags', () => {
       ' Are you sure you want to delete this databag? All data will be lost!\n'
     );
     cy.get(
-      '#mat-dialog-1 > .ng-star-inserted > .mat-dialog-actions > .mat-stroked-button > .mat-button-wrapper'
+      'app-popup-confirm > .mat-mdc-dialog-actions > .mdc-button--outlined'
     )
       .wait(deleteTimeout)
       .click();
     cy.wait(deleteTimeout);
-    cy.get('h5').should('have.text', 'Get started with Machine Learning');
+    cy.get('h1.primary').should(
+      'have.text',
+      'Get started with Machine Learning'
+    );
   });
 
   it('regression with fastlane', () => {
     cy.visit('/dashboard');
-    cy.get(
-      '.support-card > .mat-focus-indicator > .mat-button-wrapper'
-    ).click();
+    cy.get('.support-card > .mdc-button').click();
     cy.get('#mat-input-1').clear();
     cy.get('#mat-input-1').type('Fastlane Databag');
     cy.get('#file-input')
       .invoke('show')
       .selectFile('cypress/fixtures/titanic.xls');
-    cy.get('.mat-button-wrapper > .ng-star-inserted').click();
+    cy.get('#add-databag-main-button').click();
     cy.get(
-      '#cdk-step-content-0-1 > .mat-dialog-content > app-dialog-section > .dialog-element > :nth-child(3) > div'
+      '#cdk-step-content-0-1 > .mat-mdc-dialog-content > app-dialog-section > .dialog-element > :nth-child(3) > div'
     ).should('have.text', ' What do you want to predict? ');
     cy.get(
-      'app-selectable-list.ng-star-inserted > .mat-list > :nth-child(4) > .mat-list-item-content > app-list-item > div > .mat-subheading-2',
+      ':nth-child(4) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > app-list-item > .list-item-container > .mat-subtitle-2',
       { timeout: databagTimeout }
     ).should('have.text', ' numerical ');
     cy.get(
-      'app-selectable-list.ng-star-inserted > .mat-list > :nth-child(4) > .mat-list-item-content > app-list-item > div > .mat-subheading-2'
+      ':nth-child(4) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > app-list-item > .list-item-container'
     ).click();
     cy.get('#add-databag-main-button').click();
     cy.get('#mat-input-0').clear();
     cy.get('#mat-input-0').type('fast solution');
     cy.wait(1000);
     cy.get(
-      'app-choose-solver > app-selectable-list > .mat-list > .mat-list-item > .mat-list-item-content'
+      'app-choose-solver > app-selectable-list > .mat-mdc-list > .mat-mdc-list-item > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > app-list-item > .list-item-container'
     ).click();
     cy.wait(1000);
-    cy.get('.mat-button-wrapper > .ng-star-inserted').click();
+    cy.get('#add-databag-main-button').click();
     cy.get('.status-column > .done', { timeout: solutionTimeout });
   });
 
   it('dataframe script', () => {
-    cy.get('#bag-list > :nth-child(2) > .mat-list-item-content').click();
+    cy.get(
+      '#bag-list > :last-child > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended'
+    ).click();
     cy.get('#mat-input-0').clear();
     cy.get('#mat-input-0').type('script');
     cy.get('#file-input')
       .invoke('show')
       .selectFile('cypress/fixtures/dataframe_script.py');
-    cy.get('#add-databag-main-button > .mat-button-wrapper').click();
+    cy.get('#add-databag-main-button').click();
     cy.wait(1000);
     cy.get('#mat-select-value-1', { timeout: databagTimeout }).should(
       'have.text',
       'numerical'
     );
-    cy.get('#define-databag-button > .mat-button-wrapper').click();
+    cy.get('#define-databag-button').click();
     cy.get(
-      ':nth-child(1) > .mat-list-item-content > .nav-item-extended > .mat-subheading-2'
+      ':first-child > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended > .mat-subtitle-2'
     ).should('have.text', 'script');
     cy.wait(1000);
     cy.get(
-      ':nth-child(1) > .mat-list-item-content > .nav-item-extended'
+      ':nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended'
     ).click();
     cy.wait(1000);
-    cy.get('#add-solution-button-empty > .mat-button-wrapper').click();
+    cy.get('#add-solution-button-empty').click();
     cy.wait(1000);
     cy.get(
-      'app-choose-databag-column > app-selectable-list > .mat-list > :nth-child(1) > .mat-list-item-content > app-list-item > div > .on-surface-high'
+      'app-selectable-list.ng-star-inserted > .mat-mdc-list > :nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > app-list-item > .list-item-container > .primary'
     ).should('have.text', ' PassengerId ');
     cy.get(
-      'app-choose-databag-column > app-selectable-list > .mat-list > :nth-child(1) > .mat-list-item-content > app-list-item > div > .mat-subheading-2'
+      'app-selectable-list.ng-star-inserted > .mat-mdc-list > :nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > app-list-item > .list-item-container > .mat-subtitle-2'
     ).should('have.text', ' numerical ');
   });
 
   it('mnist', function () {
     cy.get(
-      ':nth-child(3) > .mat-list-item-content > .nav-item-extended'
+      ':last-child > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended'
     ).click();
     cy.get('#mat-input-0').clear();
     cy.get('#mat-input-0').type('mnist');
@@ -255,22 +257,22 @@ describe('Databags', () => {
     cy.wait(1000);
 
     cy.get(
-      '#mat-select-value-1 > .mat-select-value-text > .mat-select-min-line',
+      '#mat-select-value-1 > .mat-mdc-select-value-text > .mat-mdc-select-min-line',
       { timeout: databagTimeout }
     ).should('have.text', 'image');
     cy.get(
-      '#mat-select-value-3 > .mat-select-value-text > .mat-select-min-line',
+      '#mat-select-value-3 > .mat-mdc-select-value-text > .mat-mdc-select-min-line',
       { timeout: databagTimeout }
     ).should('have.text', 'category');
-    cy.get('#mat-dialog-0').click();
+    cy.get('#define-databag-button').click();
     cy.get(
-      ':nth-child(1) > .mat-list-item-content > .nav-item-extended > .mat-subheading-2'
+      ':nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended > .mat-subtitle-2'
     ).should('have.text', 'mnist');
   });
 
   it('tif', function () {
     cy.get(
-      ':nth-child(4) > .mat-list-item-content > .nav-item-extended'
+      ':last-child > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended'
     ).click();
     cy.get('#mat-input-0').clear();
     cy.get('#mat-input-0').type('tif');
@@ -281,28 +283,28 @@ describe('Databags', () => {
     cy.wait(1000);
 
     cy.get(
-      '#mat-select-value-1 > .mat-select-value-text > .mat-select-min-line',
+      '#mat-select-value-1 > .mat-mdc-select-value-text > .mat-mdc-select-min-line',
       { timeout: databagTimeout }
     ).should('have.text', 'image');
     cy.get(
-      '#mat-select-value-3 > .mat-select-value-text > .mat-select-min-line',
+      '#mat-select-value-3 > .mat-mdc-select-value-text > .mat-mdc-select-min-line',
       { timeout: databagTimeout }
     ).should('have.text', 'category');
-    cy.get('#define-databag-button > .mat-button-wrapper').click();
+    cy.get('#define-databag-button').click();
     cy.get(
-      ':nth-child(1) > .mat-list-item-content > .nav-item-extended > .mat-overline'
+      ':nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended'
     ).click();
-    cy.get('#add-solution-button-empty > .mat-button-wrapper').click();
+    cy.get('#add-solution-button-empty').click();
     cy.get(
-      'app-selectable-list.ng-star-inserted > .mat-list > .mat-list-item > .mat-list-item-content'
+      'app-selectable-list.ng-star-inserted > .mat-mdc-list > .mat-mdc-list-item > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > app-list-item > .list-item-container'
     ).click();
     cy.get('#define-output-next-button').click();
     cy.get('#define-solver-name-input').clear();
     cy.get('#define-solver-name-input').type('tif sol');
     cy.get(
-      'app-choose-solver > app-selectable-list > .mat-list > .mat-list-item > .mat-list-item-content'
+      'app-choose-solver > app-selectable-list > .mat-mdc-list > .mat-mdc-list-item > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > app-list-item > .list-item-container'
     ).click();
-    cy.get('#define-solver-next-button > .mat-button-wrapper').click();
+    cy.get('#define-solver-next-button').click();
     cy.get('.list-container').click();
     cy.get('.status-column > .done', { timeout: solutionTimeout });
     cy.get('#solution-status').should('have.text', ' Done ');
@@ -310,7 +312,7 @@ describe('Databags', () => {
 
   it('url', function () {
     cy.get(
-      ':nth-child(5) > .mat-list-item-content > .nav-item-extended'
+      ':last-child > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended'
     ).click();
     cy.get('#mat-input-0', { timeout: 1000 }).clear();
     cy.get('#mat-input-0').type('url');
@@ -321,16 +323,16 @@ describe('Databags', () => {
     );
     cy.get('#add-databag-main-button').click();
     cy.get(
-      '#mat-select-value-1 > .mat-select-value-text > .mat-select-min-line',
+      '#mat-select-value-1 > .mat-mdc-select-value-text > .mat-mdc-select-min-line',
       { timeout: databagTimeout }
     ).should('have.text', 'numerical');
-    cy.get('#define-databag-button > .mat-button-wrapper').click();
+    cy.get('#define-databag-button').click();
     cy.get(
-      ':nth-child(1) > .mat-list-item-content > .nav-item-extended'
+      ':nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .nav-item-extended'
     ).click();
     cy.get('#add-solution-button-empty').click();
     cy.get(
-      'app-selectable-list.ng-star-inserted > .mat-list > :nth-child(1) > .mat-list-item-content > app-list-item > div'
+      'app-selectable-list.ng-star-inserted > .mat-mdc-list > :nth-child(1) > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > app-list-item > div'
     ).should('have.text', ' PassengerId  numerical ');
   });
 });
