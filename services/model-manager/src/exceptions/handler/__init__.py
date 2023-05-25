1d59c8410820dc6b827f6973cdf75d10720414a6
Dennis Rall <dennis.rall@wogra.com>
2023-05-25 15:28:31 +0200

    Implement new routes in modelmanager

--- services/model-manager/src/exceptions/handler/__init__.py
+++ services/model-manager/src/exceptions/handler/__init__.py
@@ -1,2 +1,4 @@
+from .file_name_not_specified import file_name_not_specified_exception_handler
+from .file_not_found import file_not_found_exception_handler
 from .id_update_not_allowed import id_update_not_allowed_exception_handler
 from .resource_not_found import resource_not_found_exception_handler
