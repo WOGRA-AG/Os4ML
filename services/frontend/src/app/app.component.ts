42e0997074bc59ae387e6ca95b3d4b5e40ed28de
Max Tank <mt@wogra.com>
2023-07-05 10:21:37 +0200

    Refactor new buttons with variants and icon size

--- services/frontend/src/app/app.component.ts
+++ services/frontend/src/app/app.component.ts
@@ -13,23 +13,25 @@ import { SideNavItemComponent } from './shared/components/molecules/side-nav-ite
 import { SupportComponent } from './core/components/support/support.component';
 import { MatDialog } from '@angular/material/dialog';
 import { GettingStartedStepperComponent } from './pages/dialogs/getting-started-stepper/getting-started-stepper.component';
+import { NewButtonComponent } from "./shared/components/molecules/new-button/new-button.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  standalone: true,
-  imports: [
-    NavBarComponent,
-    RouterOutlet,
-    SideNavComponent,
-    RouterLink,
-    MaterialModule,
-    RouterLinkActive,
-    TranslateModule,
-    SideNavItemComponent,
-    SupportComponent,
-  ],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [
+        NavBarComponent,
+        RouterOutlet,
+        SideNavComponent,
+        RouterLink,
+        MaterialModule,
+        RouterLinkActive,
+        TranslateModule,
+        SideNavItemComponent,
+        SupportComponent,
+        NewButtonComponent
+    ]
 })
 export class AppComponent {
   constructor(private translate: TranslateService, private dialog: MatDialog) {
@@ -43,5 +45,8 @@ export class AppComponent {
     this.dialog.open(GettingStartedStepperComponent, {
       panelClass: 'getting-started-dialog',
     });
+  }
+  openDocumentation(): void{
+    window.open('https://wogra-ag.github.io/os4ml-docs/');
   }
 }
--- services/frontend/src/app/shared/components/molecules/new-button/new-button.component.ts
+++ services/frontend/src/app/shared/components/molecules/new-button/new-button.component.ts
@@ -2,16 +2,18 @@ import { NgClass, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-new-button',
   templateUrl: './new-button.component.html',
   styleUrls: ['./new-button.component.scss'],
   standalone: true,
-  imports: [NgIf, MatButtonModule, MatRippleModule, NgClass],
+  imports: [NgIf, MatButtonModule, MatRippleModule, NgClass, MatIconModule],
 })
 export class NewButtonComponent {
   @Input() public type: 'primary' | 'secondary' | 'text' = 'primary';
-  @Input() public text = 'Button';
+  @Input() public size: 'small' | 'medium' | 'large' = 'medium';
+  @Input() public icon = '';
   @Input() public disabled = false;
 }
