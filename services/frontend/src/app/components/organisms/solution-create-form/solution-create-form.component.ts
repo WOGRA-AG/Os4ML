a820188e2792837a8627ff130d7213ec9bf9bd5d
florian <florian@wogra.com>
2023-08-24 21:17:37 +0200

    uodate code

--- services/frontend/src/app/components/organisms/solution-create-form/solution-create-form.component.ts
+++ services/frontend/src/app/components/organisms/solution-create-form/solution-create-form.component.ts
@@ -9,25 +9,24 @@ import {
   Validators,
 } from '@angular/forms';
 import {
-  Column,
   Databag,
   Solution,
   TransferLearningModel,
+  TransferLearningSetting,
 } from '../../../../../build/openapi/modelmanager';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { ElementDividerComponent } from '../../atoms/element-divider/element-divider.component';
-import { GetPredictListItemsFromDatabagPipe } from '../../../pipes/get-predict-list-items-from-databag.pipe';
-import { TranslateModule } from '@ngx-translate/core';
-import { NgForOf, NgIf } from '@angular/common';
-import { GetDatabagByIdPipe } from '../../../pipes/get-databag-by-id.pipe';
-import { ButtonComponent } from '../../molecules/button/button.component';
-import { SolutionCreateButtonComponent } from '../solution-create-button/solution-create-button.component';
-import { NewButtonComponent } from '../../molecules/new-button/new-button.component';
 import {
   MatSlideToggleChange,
   MatSlideToggleModule,
 } from '@angular/material/slide-toggle';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ElementDividerComponent } from '../../atoms/element-divider/element-divider.component';
+import { GetDatabagByIdPipe } from '../../../pipes/get-databag-by-id.pipe';
+import { GetPredictListItemsFromDatabagPipe } from '../../../pipes/get-predict-list-items-from-databag.pipe';
+import { NgForOf, NgIf } from '@angular/common';
+import { NewButtonComponent } from '../../molecules/new-button/new-button.component';
+import { DatabagService } from '../../../services/databag.service';
 
 @Component({
   selector: 'app-solution-create-form',
@@ -35,19 +34,17 @@ import {
   styleUrls: ['./solution-create-form.component.scss'],
   standalone: true,
   imports: [
+    TranslateModule,
     MatInputModule,
-    ReactiveFormsModule,
     MatSelectModule,
     ElementDividerComponent,
-    GetPredictListItemsFromDatabagPipe,
-    TranslateModule,
-    NgIf,
-    NgForOf,
     GetDatabagByIdPipe,
-    ButtonComponent,
-    SolutionCreateButtonComponent,
-    NewButtonComponent,
+    GetPredictListItemsFromDatabagPipe,
+    ReactiveFormsModule,
+    NgForOf,
+    NgIf,
     MatSlideToggleModule,
+    NewButtonComponent,
   ],
 })
 export class SolutionCreateFormComponent implements OnInit {
@@ -58,44 +55,42 @@ export class SolutionCreateFormComponent implements OnInit {
 
   public createSolutionForm: FormGroup;
   public transferLearningSettingsActive = false;
-
   public defaultTransferLearningModel: TransferLearningModel = {
     label: 'Default Model',
     id: 'default-model',
     origin: 'OS4ML',
   };
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private databagService: DatabagService,
+    private fb: FormBuilder
+  ) {
     this.createSolutionForm = this.fb.group({
       name: ['', Validators.required],
       databagId: ['', Validators.required],
       selectedFields: ['', Validators.required],
       transferLearningSettings: this.fb.array([]),
     });
-    console.log(this.transferLearningModels);
   }
-  get name(): AbstractControl | null {
-    return this.createSolutionForm.get('name');
+  get name(): FormControl {
+    return this.createSolutionForm.get('name') as FormControl;
   }
-  get databagId(): AbstractControl | null {
-    return this.createSolutionForm.get('databagId');
+  get databagId(): FormControl {
+    return this.createSolutionForm.get('databagId') as FormControl;
   }
-  get selectedFields(): AbstractControl | null {
-    return this.createSolutionForm.get('selectedFields');
+  get selectedFields(): AbstractControl {
+    return this.createSolutionForm.get('selectedFields') as AbstractControl;
   }
   get transferLearningSettingsFormArray(): FormArray {
     return this.createSolutionForm.get('transferLearningSettings') as FormArray;
   }
   ngOnInit(): void {
-    this.createSolutionForm.get('databagId')?.setValue(this.selectedDatabagId);
-    this.populateTransferLearningSettings();
+    this.setInitialDatabagId();
+    this.updateTransferLearningSettings();
   }
-
-  getControlValue(group: AbstractControl, controlName: string): string | null {
-    const control = group.get(controlName);
-    return control instanceof FormControl ? control.value : null;
-  }
-
-  getFormControl(group: AbstractControl, controlName: string): FormControl {
+  public getFormControl(
+    group: AbstractControl,
+    controlName: string
+  ): FormControl {
     const control = group.get(controlName);
     if (control instanceof FormControl) {
       return control;
@@ -103,69 +98,73 @@ export class SolutionCreateFormComponent implements OnInit {
       throw new Error(`Control ${controlName} is not a FormControl`);
     }
   }
-
-  populateTransferLearningSettings(): void {
-    const selectedDatabag = this.databags.find(
-      databag => databag.id === this.databagId?.value
-    );
-
-    if (selectedDatabag && selectedDatabag.columns) {
-      this.transferLearningSettingsFormArray.clear(); // Clear existing form controls
-
-      selectedDatabag.columns.forEach(column => {
-        const control = this.fb.group({
-          name: column.name,
-          type: column.type,
-          selectedTransferLearningModel: this.defaultTransferLearningModel,
-        });
-        this.transferLearningSettingsFormArray.push(control);
-      });
-    }
-  }
-
-  toggleTransferLearningSettings(event: MatSlideToggleChange): void {
-    this.transferLearningSettingsActive = event.checked;
-  }
-
-  modelOfTypeExists(type: string | null): boolean {
+  public modelOfTypeExists(type: string | null): boolean {
     return this.transferLearningModels.some(model => model.type === type);
   }
-
-  getModelsByType(type: string | null): TransferLearningModel[] {
+  public getModelsByType(type: string | null): TransferLearningModel[] {
     return this.transferLearningModels.filter(model => model.type === type);
   }
-
-  public onSubmit(): void {
-    if (this.createSolutionForm.valid) {
-      const submitSolution: Solution = {
-        name: this.name?.value,
-        databagId: this.databagId?.value,
-        outputFields: this.selectedFields?.value,
-        inputFields: this.getUnselectedColumns(this.selectedFields?.value),
-      };
-      if (this.transferLearningSettingsActive) {
-        submitSolution.transferLearningSettings =
-          this.transferLearningSettingsFormArray.controls
-            .filter(
-              control =>
-                !this.selectedFields?.value.includes(
-                  this.getControlValue(control, 'name')
-                )
-            )
-            .map(control => control.value);
-      }
-      this.submitSolution.emit(submitSolution);
-    }
+  public toggleTransferLearningSettings(event: MatSlideToggleChange): void {
+    this.transferLearningSettingsActive = event.checked;
   }
-  public getUnselectedColumns(selectedFields: string[]): string[] {
-    const selectedDatabag = this.databags.find(
-      databag => databag.id === this.databagId?.value
+  public updateTransferLearningSettings(): void {
+    const settings = this.createInitialTransferLearningSettingsFromDatabag(
+      this.databagId.value
     );
-    const allFields: Column[] = selectedDatabag?.columns ?? [];
-    const allFieldsNames: string[] = allFields.map(column => column.name!);
-    const inputFields: string[] = allFieldsNames.filter(
-      name => !selectedFields.includes(name)
-    );
-    return inputFields;
+    this.setInitialTransferLearningSettings(settings);
+  }
+  public onSubmit(): void {
+    if (!this.createSolutionForm.valid) return;
+    const submitSolution: Solution = {
+      name: this.name.value,
+      databagId: this.databagId.value,
+      outputFields: this.selectedFields.value,
+      inputFields: this.getUnselectedColumns(this.selectedFields.value),
+    };
+    if (this.transferLearningSettingsActive) {
+      submitSolution.transferLearningSettings =
+        this.getTransferLearningSettings();
+    }
+    this.submitSolution.emit(submitSolution);
+  }
+  private setInitialDatabagId(): void {
+    this.createSolutionForm.get('databagId')?.setValue(this.selectedDatabagId);
+  }
+  private createInitialTransferLearningSettingsFromDatabag(
+    databagId: string
+  ): TransferLearningSetting[] {
+    const selectedDatabag = this.databagService.getDatabagById(databagId);
+    if (!selectedDatabag?.columns) return [];
+
+    return selectedDatabag.columns.map(column => ({
+      name: column.name,
+      type: column.type,
+      selectedTransferLearningModel: this.defaultTransferLearningModel,
+    }));
+  }
+  private setInitialTransferLearningSettings(
+    settings: TransferLearningSetting[]
+  ): void {
+    const settingsArray = this.transferLearningSettingsFormArray;
+    settingsArray.clear();
+    settings.forEach(setting => {
+      const control = this.fb.group(setting);
+      settingsArray.push(control);
+    });
+  }
+  private getTransferLearningSettings(): TransferLearningSetting[] {
+    const filteredControls =
+      this.transferLearningSettingsFormArray.controls.filter(
+        control =>
+          !this.selectedFields?.value.includes(control.get('name')?.value)
+      );
+    return filteredControls.map(control => control.value);
+  }
+  private getUnselectedColumns(selectedColumns: string[]): string[] {
+    const allColumnNames =
+      this.databagService
+        .getDatabagById(this.databagId.value)
+        ?.columns?.map(column => column.name!) ?? [];
+    return allColumnNames.filter(name => !selectedColumns.includes(name));
   }
 }
--- services/frontend/src/app/components/organisms/solution-transfer-learning-settings/solution-transfer-learning-settings.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-solution-transfer-learning-settings',
-  templateUrl: './solution-transfer-learning-settings.component.html',
-  styleUrls: ['./solution-transfer-learning-settings.component.scss'],
-})
-export class SolutionTransferLearningSettingsComponent {}
