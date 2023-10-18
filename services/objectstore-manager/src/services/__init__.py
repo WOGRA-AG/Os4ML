ca4d61e70d68e13f70d5e738504f2e7554913967
Dennis Rall <dennis.rall@wogra.com>
2023-10-18 11:05:27 +0200

    Cleanup init files

--- services/objectstore-manager/src/services/__init__.py
+++ services/objectstore-manager/src/services/__init__.py
@@ -1,5 +1,4 @@
 import os
-from datetime import timedelta
 
 STORAGE_BACKEND: str = os.getenv(
     "OBJECTSTORE_STORAGE_BACKEND", default="minio"
@@ -22,25 +21,3 @@ STORAGE_SECURE: bool = (
 )
 
 BUCKET_NAME: str = os.getenv("OS4ML_BUCKET_NAME", default="os4ml")
-
-DATABAG_CONFIG_FILE_NAME: str = os.getenv(
-    "DATABAG_CONFIG_FILE_NAME", default="databag.json"
-)
-
-SOLUTION_CONFIG_FILE_NAME: str = os.getenv(
-    "SOLUTION_CONFIG_FILE_NAME", default="solution.json"
-)
-
-TEMPLATE_METADATA_FILE_NAME: str = os.getenv(
-    "TEMPLATE_METADATA_FILE_NAME", default="metadata.json"
-)
-
-COMPONENT_FILE_NAME: str = os.getenv(
-    "COMPONENT_FILE_NAME", default="component.yaml"
-)
-
-PIPELINE_FILE_NAME: str = os.getenv(
-    "PIPELINE_FILE_NAME", default="pipeline.yaml"
-)
-
-EXPIRY: timedelta = timedelta(hours=2)
