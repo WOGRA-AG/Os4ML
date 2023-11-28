2d27e9c0acadaa80df1587db93d72682ff9b8742
franzi_fk <franziska@wogra.com>
2023-11-28 08:44:27 +0100

    change color of context-menu-item

--- services/frontend/src/app/components/molecules/context-menu-item/context-menu-item.component.ts
+++ services/frontend/src/app/components/molecules/context-menu-item/context-menu-item.component.ts
@@ -3,7 +3,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { NgClass } from '@angular/common';
-import { MatRippleModule } from '@angular/material/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -11,22 +10,11 @@ import { RouterLink } from '@angular/router';
   templateUrl: './context-menu-item.component.html',
   styleUrls: ['./context-menu-item.component.scss'],
   standalone: true,
-  imports: [
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule,
-    NgClass,
-    MatRippleModule,
-    RouterLink,
-  ],
+  imports: [MatButtonModule, MatIconModule, MatMenuModule, NgClass, RouterLink],
 })
 export class ContextMenuItemComponent {
-  @Input() public disabled = false;
   @Input() public menuDescription = '';
   @Input() public menuIcon = '';
   @Input() public routerLink: string[] | string = [];
   @Input() public params = {};
-  @Input() public variant: 'primary' | 'secondary' | 'text' | 'warn' =
-    'primary';
-  @Input() public size: 'small' | 'medium' | 'large' = 'medium';
 }
