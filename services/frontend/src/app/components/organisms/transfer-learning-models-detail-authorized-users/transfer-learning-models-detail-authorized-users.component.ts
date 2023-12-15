f7eca5ff7c6b0a5dbd41b576f0af8a905e65258f
florian <florian@wogra.com>
2023-12-15 23:01:07 +0100

    flo changes

--- services/frontend/src/app/components/organisms/transfer-learning-models-detail-authorized-users/transfer-learning-models-detail-authorized-users.component.ts
+++ services/frontend/src/app/components/organisms/transfer-learning-models-detail-authorized-users/transfer-learning-models-detail-authorized-users.component.ts
@@ -1,11 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { SolutionContextMenuComponent } from '../solution-context-menu/solution-context-menu.component';
 import { NewButtonComponent } from '../../molecules/new-button/new-button.component';
 import { IconButtonComponent } from '../../molecules/icon-button/icon-button.component';
 import { JsonPipe } from '@angular/common';
+import {
+  ModelShare,
+} from '../../../../../build/openapi/modelmanager';
 
 @Component({
   selector: 'app-transfer-learning-models-detail-authorized-users',
@@ -28,7 +31,12 @@ import { JsonPipe } from '@angular/common';
 export class TransferLearningModelsDetailAuthorizedUsersComponent {
   @Output() public addAuthorizedUser = new EventEmitter<void>();
   @Output() public removeAuthorizedUser = new EventEmitter<string>();
-  @Input() public sharedWith: string[] = [];
+  @Input() public modelShare: ModelShare[] = [];
 
   public displayedColumns: string[] = ['id', 'actions'];
+  public dataSource: MatTableDataSource<ModelShare>;
+
+  constructor() {
+    this.dataSource = new MatTableDataSource<ModelShare>(this.modelShare);
+  }
 }
