f936d470619d8a27a31a3d3988dd3a3f2f8ee463
florian <florian@wogra.com>
2023-12-20 11:16:16 +0100

    update

--- services/frontend/src/app/components/organisms/transfer-learning-model-context-menu/transfer-learning-model-context-menu.component.ts
+++ services/frontend/src/app/components/organisms/transfer-learning-model-context-menu/transfer-learning-model-context-menu.component.ts
@@ -6,6 +6,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { ContextMenuItemComponent } from '../../molecules/context-menu-item/context-menu-item.component';
 import { IconButtonComponent } from '../../molecules/icon-button/icon-button.component';
 import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
+import { StringToTLMOriginPipe } from '../../../pipes/string-to-tlmorigin.pipe';
+import { TransferLearningModelsDetailDeleteModelComponent } from '../transfer-learning-models-detail-delete-model/transfer-learning-models-detail-delete-model.component';
 
 @Component({
   selector: 'app-transfer-learning-model-context-menu',
@@ -19,9 +22,13 @@ import { MatButtonModule } from '@angular/material/button';
     MatMenuModule,
     MatIconModule,
     ContextMenuItemComponent,
+    NgIf,
+    StringToTLMOriginPipe,
+    TransferLearningModelsDetailDeleteModelComponent,
   ],
 })
 export class TransferLearningModelContextMenuComponent {
+  @Input() public deleteTransferLearningModelDisabled = true;
   @Input() public showTransferLearningModelDetailParams: Params = {};
   @Input() public showTransferLearningModelDetailLink: string[] | string = [];
   @Output() public deleteTransferLearningModelButton =
--- services/frontend/src/app/components/organisms/transfer-learning-models-table/transfer-learning-models-table.component.ts
+++ services/frontend/src/app/components/organisms/transfer-learning-models-table/transfer-learning-models-table.component.ts
@@ -4,6 +4,9 @@ import { IconButtonComponent } from '../../molecules/icon-button/icon-button.com
 import { MatTableModule } from '@angular/material/table';
 import { TranslateModule } from '@ngx-translate/core';
 import { TransferLearningModelContextMenuComponent } from '../transfer-learning-model-context-menu/transfer-learning-model-context-menu.component';
+import { TransferLearningModelOrigins } from '../../../models/transfer-learning-model-origins';
+import { NgIf } from '@angular/common';
+import { StringToTLMOriginPipe } from '../../../pipes/string-to-tlmorigin.pipe';
 
 @Component({
   selector: 'app-transfer-learning-models-table',
@@ -14,6 +17,8 @@ import { TransferLearningModelContextMenuComponent } from '../transfer-learning-
     MatTableModule,
     TranslateModule,
     TransferLearningModelContextMenuComponent,
+    NgIf,
+    StringToTLMOriginPipe,
   ],
   standalone: true,
 })
@@ -22,4 +27,6 @@ export class TransferLearningModelsTableComponent {
     new EventEmitter<string>();
   @Input() public transferLearningModels: TransferLearningModel[] = [];
   public displayedColumns: string[] = ['label', 'type', 'origin', 'actions'];
+  protected readonly TransferLearningModelOrigins =
+    TransferLearningModelOrigins;
 }
